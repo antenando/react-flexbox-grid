9dc47b43896ea0e498b9cd420e36b34d780e1488
Fernando Bueno <antenand@gmail.com>
2020-02-25 00:36:22 -0500

    removes dist

--- src/classNames.js
+++ src/classNames.js
@@ -1,4 +1,4 @@
-import styles from '@antenando/flexboxgrid2/dist/flexboxgrid2.css';
+import styles from '@antenando/flexboxgrid2/flexboxgrid2.css';
 
 export default function getClass(className) {
   return (styles && styles[className]) ? styles[className] : className;
